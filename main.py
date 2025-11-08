71c2d3f9787cd9e4e909f48cefd70ff99772a97e
Junior Eduardo Garniga Rojas <jgarnigar@miumg.edu.gt>
2025-11-08 02:34:15 -0600

    agregamos __main__

--- main.py
+++ main.py
@@ -23,19 +23,29 @@ create = crear_array()
 
 inversa = cir.inversa(matriz_codificacion)
 
+def main():
+    try: 
+        valores_x, valores_y = aplicacion.abrir_document(matriz_codificacion, "datos/datos encriptados.txt", "descifrar")
+        desempaquetar_x, desempaquetar_y = desempaquetador.desempaquetar(valores_x, valores_y)
 
-valores_x, valores_y = aplicacion.abrir_document(matriz_codificacion, "datos/datos encriptados.txt", "descifrar")
-desempaquetar_x, desempaquetar_y = desempaquetador.desempaquetar(valores_x, valores_y)
+        valores_rotados_x, valores_rotados_y = rotar.rotar_matriz(desempaquetar_x, desempaquetar_y)
 
-valores_rotados_x, valores_rotados_y = rotar.rotar_matriz(desempaquetar_x, desempaquetar_y)
+        valores_trasladados_x, valores_trasladados_y = traslacion.trasladar_matriz(valores_rotados_x, valores_rotados_y, 20, 30)
 
-valores_trasladados_x, valores_trasladados_y = traslacion.trasladar_matriz(valores_rotados_x, valores_rotados_y, 20, 30)
-
-graficar.graficadora(desempaquetar_x, desempaquetar_y)
-graficar.graficadora(valores_rotados_x, valores_rotados_y)
-graficar.graficadora(valores_trasladados_x, valores_trasladados_y)
+        graficar.graficadora(desempaquetar_x, desempaquetar_y)
+        graficar.graficadora(valores_rotados_x, valores_rotados_y)
+        graficar.graficadora(valores_trasladados_x, valores_trasladados_y)
 
 
-guardar_datos.save(desempaquetar_x, desempaquetar_y, "datos/valores desencriptados.txt")
-guardar_datos.save(valores_rotados_x, valores_rotados_y, "datos/valores rotados.txt")
-guardar_datos.save(valores_trasladados_x,valores_trasladados_y,"datos/valores trasladados.txt")
+        guardar_datos.save(desempaquetar_x, desempaquetar_y, "datos/valores desencriptados.txt")
+        guardar_datos.save(valores_rotados_x, valores_rotados_y, "datos/valores rotados.txt")
+        guardar_datos.save(valores_trasladados_x,valores_trasladados_y,"datos/valores trasladados.txt")
+
+        return True
+    except Exception as e:
+        print(f"Error encontrado: {e}")
+
+
+if __name__ == "__main__":
+    main()
+    
